3b7e01c8be23067107ff75e00ae22b66bf28b662
Dustin L. Howett <dustin@howett.net>
2016-05-30 16:35:58 -0700

    [Clipper] Add the new Oakland Airport transfer station.
    Thanks @DHowett!
    Closes #93

--- src/main/java/com/codebutler/farebot/transit/clipper/ClipperData.java
+++ src/main/java/com/codebutler/farebot/transit/clipper/ClipperData.java
@@ -85,13 +85,13 @@ final class ClipperData {
         .put(0x17L, new Station("Ashby Station",                       "Ashby",                "37.85303",  "-122.269965"))
         .put(0x18L, new Station("Downtown Berkeley Station",           "Berkeley",             "37.869868", "-122.268051"))
         .put(0x19L, new Station("North Berkeley Station",              "North Berkeley",       "37.874026", "-122.283882"))
-        .put(0x20L, new Station("Coliseum/Oakland Airport BART",       "Coliseum/OAK",         "37.754270", "-122.197757"))
+        .put(0x20L, new Station("Coliseum Station",                    "Coliseum",             "37.754270", "-122.197757"))
         .put(0x1aL, new Station("El Cerrito Plaza Station",            "El Cerrito Plaza",     "37.903959", "-122.299271"))
         .put(0x1bL, new Station("El Cerrito Del Norte Station",        "El Cerrito Del Norte", "37.925651", "-122.317219"))
         .put(0x1cL, new Station("Richmond Station",                    "Richmond",             "37.93730",  "-122.35338"))
         .put(0x1dL, new Station("Lake Merritt Station",                "Lake Merritt",         "37.79761",  "-122.26564"))
         .put(0x1eL, new Station("Fruitvale Station",                   "Fruitvale",            "37.77495",  "-122.22425"))
-        .put(0x1fL, new Station("Coliseum/Oakland Airport Station",    "Coliseum/OAK",         "37.75256",  "-122.19806"))
+        .put(0x1fL, new Station("Coliseum Station",                    "Coliseum",             "37.75256",  "-122.19806"))
         .put(0x22L, new Station("Hayward Station",                     "Hayward",              "37.670387", "-122.088002"))
         .put(0x23L, new Station("South Hayward Station",               "South Hayward",        "37.634800", "-122.057551"))
         .put(0x24L, new Station("Union City Station",                  "Union City",           "37.591203", "-122.017854"))
@@ -102,6 +102,7 @@ final class ClipperData {
         .put(0x2aL, new Station("San Francisco Int'l Airport Station", "SFO",                  "37.61590",  "-122.39263"))
         .put(0x2bL, new Station("Millbrae Station",                    "Millbrae",             "37.599935", "-122.386478"))
         .put(0x2cL, new Station("West Dublin/Pleasanton Station",      "W. Dublin/Pleasanton", "37.699764", "-121.928118"))
+        .put(0x2dL, new Station("Oakland Airport Station",             "OAK Airport",          "37.75256",  "-122.19806"))
         .build();
 
     static final Map<Long, String> GG_FERRY_ROUTES = new ImmutableMapBuilder<Long, String>()
