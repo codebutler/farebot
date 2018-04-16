c59b33aafef86d2b3e5d9d41b912fd2c53791e90
Zac Sweers <zac.sweers@gmail.com>
2018-04-16 14:40:14 +0200

    Target types

--- farebot-transit-registry-annotations/src/main/java/com/codebutler/farebot/transit/registry/annotations/TransitCardRegistry.java
+++ farebot-transit-registry-annotations/src/main/java/com/codebutler/farebot/transit/registry/annotations/TransitCardRegistry.java
@@ -1,7 +1,14 @@
 package com.codebutler.farebot.transit.registry.annotations;
 
 import com.uber.crumb.annotations.CrumbConsumer;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
 
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.CLASS;
+
+@Target(TYPE)
+@Retention(CLASS)
 @CrumbConsumer
 public @interface TransitCardRegistry {
 }
