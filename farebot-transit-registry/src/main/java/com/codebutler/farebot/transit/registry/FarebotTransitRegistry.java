14309e1b9261b739be1f66346e0103dbf212f195
Zac Sweers <zac.sweers@gmail.com>
2018-04-16 17:14:16 +0200

    Add getter

--- farebot-transit-registry/src/main/java/com/codebutler/farebot/transit/registry/FarebotTransitRegistry.java
+++ farebot-transit-registry/src/main/java/com/codebutler/farebot/transit/registry/FarebotTransitRegistry.java
@@ -1,8 +1,16 @@
 package com.codebutler.farebot.transit.registry;
 
+import android.content.Context;
+import com.codebutler.farebot.card.Card;
+import com.codebutler.farebot.transit.TransitFactory;
+import com.codebutler.farebot.transit.TransitInfo;
 import com.codebutler.farebot.transit.registry.annotations.TransitCardRegistry;
+import java.util.List;
+import java.util.Map;
 
 @TransitCardRegistry
 public abstract class FarebotTransitRegistry {
-
+  public static Map<Class<? extends Card>, List<TransitFactory<Card, TransitInfo>>> get(Context context) {
+    return CardsRegistry_FarebotTransitRegistry.create();
+  }
 }
