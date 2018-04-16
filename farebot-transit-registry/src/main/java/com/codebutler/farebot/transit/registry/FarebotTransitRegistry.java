58cf44d75214fcc2e6974b8c622d3301a886f9f2
Zac Sweers <zac.sweers@gmail.com>
2018-04-16 17:12:01 +0200

    Add registry

---
+++ farebot-transit-registry/src/main/java/com/codebutler/farebot/transit/registry/FarebotTransitRegistry.java
@@ -0,0 +1,8 @@
+package com.codebutler.farebot.transit.registry;
+
+import com.codebutler.farebot.transit.registry.annotations.TransitCardRegistry;
+
+@TransitCardRegistry
+public abstract class FarebotTransitRegistry {
+
+}
