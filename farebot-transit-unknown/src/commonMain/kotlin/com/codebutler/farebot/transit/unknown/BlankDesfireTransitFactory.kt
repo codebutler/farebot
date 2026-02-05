9319267507ce62dd8cce055e0daa9c3ff16e33d2
Eric Butler <eric@codebutler.com>
2026-02-05 08:53:58 -0500

    Port unknown card handler from Metrodroid
    Handler for blank or unrecognized NFC cards.
    
    Ported from Metrodroid (https://github.com/metrodroid/metrodroid)
    
    Co-Authored-By: Michael Farrell <micolous+git@gmail.com>

---
+++ farebot-transit-unknown/src/commonMain/kotlin/com/codebutler/farebot/transit/unknown/BlankDesfireTransitFactory.kt
@@ -0,0 +1,72 @@
+/*
+ * BlankDesfireTransitFactory.kt
+ *
+ * Copyright 2018 Google
+ * Copyright 2025 Eric Butler <eric@codebutler.com>
+ *
+ * Ported from Metrodroid (https://github.com/metrodroid/metrodroid)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.codebutler.farebot.transit.unknown
+
+import com.codebutler.farebot.base.ui.HeaderListItem
+import com.codebutler.farebot.base.ui.ListItem
+import com.codebutler.farebot.base.ui.ListItemInterface
+import com.codebutler.farebot.card.desfire.DesfireCard
+import com.codebutler.farebot.transit.TransitFactory
+import com.codebutler.farebot.transit.TransitIdentity
+import com.codebutler.farebot.transit.TransitInfo
+import farebot.farebot_transit_unknown.generated.resources.*
+import kotlinx.coroutines.runBlocking
+import org.jetbrains.compose.resources.getString
+
+/**
+ * Detects blank MIFARE DESFire cards with no applications.
+ * This factory should be registered near the END of the DESFire factory list,
+ * just before UnauthorizedDesfireTransitFactory.
+ */
+class BlankDesfireTransitFactory : TransitFactory<DesfireCard, BlankDesfireTransitInfo> {
+
+    /**
+     * @param card Card to read.
+     * @return true if the card has no applications (blank DESFire).
+     */
+    override fun check(card: DesfireCard): Boolean {
+        // A blank DESFire card has no applications
+        return card.applications.isEmpty()
+    }
+
+    override fun parseIdentity(card: DesfireCard): TransitIdentity {
+        val name = runBlocking { getString(Res.string.blank_mfd_card) }
+        return TransitIdentity.create(name, null)
+    }
+
+    override fun parseInfo(card: DesfireCard): BlankDesfireTransitInfo {
+        return BlankDesfireTransitInfo()
+    }
+}
+
+class BlankDesfireTransitInfo : TransitInfo() {
+    override val cardName: String = runBlocking { getString(Res.string.blank_mfd_card) }
+
+    override val serialNumber: String? = null
+
+    override val info: List<ListItemInterface>
+        get() = listOf(
+            HeaderListItem(Res.string.fully_blank_title),
+            ListItem(Res.string.fully_blank_desc)
+        )
+}
---
+++ farebot-transit-unknown/src/commonMain/kotlin/com/codebutler/farebot/transit/unknown/UnauthorizedClassicTransitFactory.kt
@@ -0,0 +1,87 @@
+/*
+ * UnauthorizedClassicTransitFactory.kt
+ *
+ * Copyright 2015-2018 Michael Farrell <micolous+git@gmail.com>
+ * Copyright 2025 Eric Butler <eric@codebutler.com>
+ *
+ * Ported from Metrodroid (https://github.com/metrodroid/metrodroid)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.codebutler.farebot.transit.unknown
+
+import com.codebutler.farebot.base.ui.HeaderListItem
+import com.codebutler.farebot.base.ui.ListItem
+import com.codebutler.farebot.base.ui.ListItemInterface
+import com.codebutler.farebot.card.classic.ClassicCard
+import com.codebutler.farebot.card.classic.UnauthorizedClassicSector
+import com.codebutler.farebot.transit.TransitFactory
+import com.codebutler.farebot.transit.TransitIdentity
+import com.codebutler.farebot.transit.TransitInfo
+import farebot.farebot_transit_unknown.generated.resources.*
+import kotlinx.coroutines.runBlocking
+import kotlinx.serialization.Serializable
+import org.jetbrains.compose.resources.getString
+
+/**
+ * Catch-all for MIFARE Classic cards where all sectors are locked/unauthorized.
+ * This factory should be registered LAST in the Classic factory list.
+ */
+class UnauthorizedClassicTransitFactory : TransitFactory<ClassicCard, UnauthorizedClassicTransitInfo> {
+
+    /**
+     * This should be the last executed MIFARE Classic check, after all the other checks are done.
+     * This is because it will catch others' cards.
+     *
+     * @param card Card to read.
+     * @return true if all sectors on the card are locked.
+     */
+    override fun check(card: ClassicCard): Boolean {
+        // Check if ALL sectors are unauthorized (completely locked card)
+        return card.sectors.all { it is UnauthorizedClassicSector }
+    }
+
+    override fun parseIdentity(card: ClassicCard): TransitIdentity {
+        val name = runBlocking { getString(Res.string.locked_mfc_card) }
+        return TransitIdentity.create(name, null)
+    }
+
+    override fun parseInfo(card: ClassicCard): UnauthorizedClassicTransitInfo {
+        // Standard MIFARE Classic cards can be unlocked with keys
+        // MIFARE Plus / DESFire emulation cannot be unlocked this way
+        val isUnlockable = true  // TODO: Detect MIFARE Plus when subType info is available
+        return UnauthorizedClassicTransitInfo(isUnlockable = isUnlockable)
+    }
+}
+
+@Serializable
+data class UnauthorizedClassicTransitInfo(
+    val isUnlockable: Boolean = false
+) : TransitInfo() {
+    override val cardName: String = runBlocking { getString(Res.string.locked_mfc_card) }
+
+    override val serialNumber: String? = null
+
+    override val info: List<ListItemInterface>
+        get() = listOf(
+            HeaderListItem(Res.string.fully_locked_title),
+            ListItem(
+                if (isUnlockable)
+                    Res.string.fully_locked_desc_unlockable
+                else
+                    Res.string.fully_locked_desc
+            )
+        )
+}
