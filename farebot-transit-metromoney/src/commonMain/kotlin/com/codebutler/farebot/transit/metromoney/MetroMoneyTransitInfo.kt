d9f63570fd092a6eef842b7a76ce30e55342a5d6
Eric Butler <eric@codebutler.com>
2026-02-05 08:53:59 -0500

    Port MetroMoney transit system from Metrodroid (Georgia)
    Ported from Metrodroid (https://github.com/metrodroid/metrodroid)

---
+++ farebot-transit-metromoney/src/commonMain/kotlin/com/codebutler/farebot/transit/metromoney/MetroMoneyTransitInfo.kt
@@ -0,0 +1,66 @@
+/*
+ * MetroMoneyTransitInfo.kt
+ *
+ * Copyright 2018 Google
+ *
+ * Ported from Metrodroid (https://github.com/metrodroid/metrodroid)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.codebutler.farebot.transit.metromoney
+
+import com.codebutler.farebot.base.ui.ListItem
+import com.codebutler.farebot.base.ui.ListItemInterface
+import com.codebutler.farebot.base.util.NumberUtils
+import com.codebutler.farebot.transit.TransitBalance
+import com.codebutler.farebot.transit.TransitCurrency
+import com.codebutler.farebot.transit.TransitInfo
+import farebot.farebot_transit_metromoney.generated.resources.Res
+import farebot.farebot_transit_metromoney.generated.resources.card_name_metromoney
+import farebot.farebot_transit_metromoney.generated.resources.metromoney_date1
+import farebot.farebot_transit_metromoney.generated.resources.metromoney_date2
+import farebot.farebot_transit_metromoney.generated.resources.metromoney_date3
+import farebot.farebot_transit_metromoney.generated.resources.metromoney_date4
+import kotlinx.coroutines.runBlocking
+import org.jetbrains.compose.resources.getString
+
+class MetroMoneyTransitInfo(
+    private val mSerial: Long,
+    private val mBalance: Int,
+    private val mDate1: String,
+    private val mDate2: String,
+    private val mDate3: String,
+    private val mDate4: String
+) : TransitInfo() {
+
+    override val serialNumber: String
+        get() = NumberUtils.zeroPad(mSerial, 10)
+
+    override val cardName: String
+        get() = runBlocking { getString(Res.string.card_name_metromoney) }
+
+    override val balance: TransitBalance
+        get() = TransitBalance(balance = TransitCurrency(mBalance, "GEL"))
+
+    override val info: List<ListItemInterface>?
+        get() {
+            val items = mutableListOf<ListItemInterface>()
+            if (mDate1.isNotEmpty()) items.add(ListItem(Res.string.metromoney_date1, mDate1))
+            if (mDate2.isNotEmpty()) items.add(ListItem(Res.string.metromoney_date2, mDate2))
+            if (mDate3.isNotEmpty()) items.add(ListItem(Res.string.metromoney_date3, mDate3))
+            if (mDate4.isNotEmpty()) items.add(ListItem(Res.string.metromoney_date4, mDate4))
+            return items.ifEmpty { null }
+        }
+}
