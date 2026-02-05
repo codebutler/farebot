a2f04231d672cbb8f5df6e4b5fd73b634c289ae0
Eric Butler <eric@codebutler.com>
2026-02-05 08:53:59 -0500

    Port Kyiv Transit transit system from Metrodroid (Kyiv, Ukraine)
    Ported from Metrodroid (https://github.com/metrodroid/metrodroid)

---
+++ farebot-transit-kiev/src/commonMain/kotlin/com/codebutler/farebot/transit/kiev/KievTransitFactory.kt
@@ -0,0 +1,74 @@
+/*
+ * KievTransitFactory.kt
+ *
+ * Copyright 2018 Google
+ *
+ * Ported from Metrodroid (https://github.com/metrodroid/metrodroid)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.codebutler.farebot.transit.kiev
+
+import com.codebutler.farebot.base.util.HashUtils
+import com.codebutler.farebot.base.util.byteArrayToInt
+import com.codebutler.farebot.base.util.hex
+import com.codebutler.farebot.base.util.reverseBuffer
+import com.codebutler.farebot.base.util.sliceOffLen
+import com.codebutler.farebot.card.classic.ClassicCard
+import com.codebutler.farebot.card.classic.DataClassicSector
+import com.codebutler.farebot.transit.TransitFactory
+import com.codebutler.farebot.transit.TransitIdentity
+
+class KievTransitFactory : TransitFactory<ClassicCard, KievTransitInfo> {
+
+    override fun check(card: ClassicCard): Boolean {
+        val sector1 = card.getSector(1) as? DataClassicSector ?: return false
+        return HashUtils.checkKeyHash(
+            sector1.keyA, sector1.keyB, "kiev",
+            "902a69a9d68afa1ddac7b61a512f7d4f"
+        ) >= 0
+    }
+
+    override fun parseIdentity(card: ClassicCard): TransitIdentity {
+        return TransitIdentity.create(
+            KievTransitInfo.NAME,
+            KievTransitInfo.formatSerial(getSerial(card))
+        )
+    }
+
+    override fun parseInfo(card: ClassicCard): KievTransitInfo {
+        return KievTransitInfo(
+            mSerial = getSerial(card),
+            trips = parseTrips(card)
+        )
+    }
+
+    companion object {
+        private fun parseTrips(card: ClassicCard): List<KievTrip> =
+            (0..5).mapNotNull { i ->
+                val sector = card.getSector(3 + i / 3) as? DataClassicSector ?: return@mapNotNull null
+                val data = sector.getBlock(i % 3).data
+                if (data.byteArrayToInt(0, 4) == 0) null else KievTrip(data)
+            }
+
+        private fun getSerial(card: ClassicCard): String {
+            val sector1 = card.getSector(1) as DataClassicSector
+            return sector1.getBlock(0).data
+                .sliceOffLen(6, 8)
+                .reverseBuffer()
+                .hex()
+        }
+    }
+}
