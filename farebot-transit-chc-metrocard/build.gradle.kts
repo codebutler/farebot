f4576a804429804d92fdbfab060d7cccf7732ff4
Eric Butler <eric@codebutler.com>
2026-02-05 08:54:00 -0500

    Port CHC Metrocard transit system from Metrodroid (Christchurch, New Zealand)
    Ported from Metrodroid (https://github.com/metrodroid/metrodroid)
    
    Co-Authored-By: Michael Farrell <micolous+git@gmail.com>

---
+++ farebot-transit-chc-metrocard/build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.android.kotlin.multiplatform.library)
+    alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.compose.multiplatform)
+    alias(libs.plugins.kotlin.compose)
+}
+kotlin {
+    androidLibrary {
+        namespace = "com.codebutler.farebot.transit.chc_metrocard"
+        compileSdk = libs.versions.compileSdk.get().toInt()
+        minSdk = libs.versions.minSdk.get().toInt()
+    }
+    iosX64()
+    iosArm64()
+    iosSimulatorArm64()
+    sourceSets {
+        commonMain.dependencies {
+            implementation(compose.components.resources)
+            implementation(compose.runtime)
+            implementation(project(":farebot-base"))
+            implementation(project(":farebot-transit"))
+            implementation(project(":farebot-card-classic"))
+            implementation(project(":farebot-transit-erg"))
+            implementation(libs.kotlinx.serialization.json)
+            implementation(libs.kotlinx.datetime)
+        }
+    }
+}
---
+++ farebot-transit-chc-metrocard/src/commonMain/kotlin/com/codebutler/farebot/transit/chc_metrocard/ChcMetrocardTransitFactory.kt
@@ -0,0 +1,76 @@
+/*
+ * ChcMetrocardTransitFactory.kt
+ *
+ * Copyright 2018-2019 Michael Farrell <micolous+git@gmail.com>
+ *
+ * Ported from Metrodroid (https://github.com/metrodroid/metrodroid)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.codebutler.farebot.transit.chc_metrocard
+
+import com.codebutler.farebot.card.classic.ClassicCard
+import com.codebutler.farebot.card.classic.DataClassicSector
+import com.codebutler.farebot.transit.TransitFactory
+import com.codebutler.farebot.transit.TransitIdentity
+import com.codebutler.farebot.transit.erg.ErgTransitInfo
+import farebot.farebot_transit_chc_metrocard.generated.resources.Res
+import farebot.farebot_transit_chc_metrocard.generated.resources.chc_metrocard_card_name
+import kotlinx.coroutines.runBlocking
+import org.jetbrains.compose.resources.getString
+
+/**
+ * Factory for detecting and parsing CHC Metrocard (Christchurch, NZ) transit cards.
+ *
+ * This is an ERG-based card identified by the ERG signature and agency ID 0x0136.
+ */
+class ChcMetrocardTransitFactory : TransitFactory<ClassicCard, ChcMetrocardTransitInfo> {
+
+    override fun check(card: ClassicCard): Boolean {
+        val sector0 = card.getSector(0)
+        if (sector0 !is DataClassicSector) return false
+        val file1 = sector0.getBlock(1).data
+        if (file1.size < ErgTransitInfo.SIGNATURE.size) return false
+
+        if (!file1.copyOfRange(0, ErgTransitInfo.SIGNATURE.size)
+                .contentEquals(ErgTransitInfo.SIGNATURE)) {
+            return false
+        }
+
+        val metadata = ErgTransitInfo.getMetadataRecord(card)
+        return metadata != null && metadata.agencyId == ChcMetrocardTransitInfo.AGENCY_ID
+    }
+
+    override fun parseIdentity(card: ClassicCard): TransitIdentity {
+        val metadata = ErgTransitInfo.getMetadataRecord(card)
+        val serial = metadata?.cardSerial?.let { s ->
+            var result = 0
+            for (b in s) {
+                result = (result shl 8) or (b.toInt() and 0xFF)
+            }
+            result.toString()
+        }
+        return TransitIdentity.create(runBlocking { getString(Res.string.chc_metrocard_card_name) }, serial)
+    }
+
+    override fun parseInfo(card: ClassicCard): ChcMetrocardTransitInfo {
+        val capsule = ErgTransitInfo.parse(
+            card,
+            newTrip = { purse, epoch -> ChcMetrocardTrip(purse, epoch) },
+            newRefill = { purse, epoch -> ChcMetrocardRefill(purse, epoch) }
+        )
+        return ChcMetrocardTransitInfo(capsule)
+    }
+}
