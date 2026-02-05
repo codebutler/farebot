3592787bc6eb9858d355a732a1d6fc637c3e001d
Eric Butler <eric@codebutler.com>
2026-02-05 08:53:59 -0500

    Port Otago transit system from Metrodroid (Otago, New Zealand)
    Ported from Metrodroid (https://github.com/metrodroid/metrodroid)

---
+++ farebot-transit-otago/src/commonMain/kotlin/com/codebutler/farebot/transit/otago/OtagoGoCardTrip.kt
@@ -0,0 +1,42 @@
+/*
+ * OtagoGoCardTrip.kt
+ *
+ * Copyright 2018 Google
+ * Copyright 2025 Eric Butler <eric@codebutler.com>
+ *
+ * Ported from Metrodroid (https://github.com/metrodroid/metrodroid)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.codebutler.farebot.transit.otago
+
+import com.codebutler.farebot.transit.TransitCurrency
+import com.codebutler.farebot.transit.Trip
+import kotlin.time.Instant
+
+class OtagoGoCardTrip(
+    private val timestamp: Instant,
+    private val cost: Int,
+    private val machine: String
+) : Trip() {
+
+    override val startTimestamp: Instant get() = timestamp
+
+    override val fare: TransitCurrency get() = TransitCurrency.NZD(cost)
+
+    override val mode: Mode get() = Mode.BUS
+
+    override val machineID: String get() = machine
+}
