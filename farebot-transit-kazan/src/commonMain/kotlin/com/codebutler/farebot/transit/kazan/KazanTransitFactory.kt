20236f16b6fcd8513501cac0007b5d50e6a39bbf
Eric Butler <eric@codebutler.com>
2026-02-05 08:53:59 -0500

    Port Kazan Transit transit system from Metrodroid (Kazan, Russia)
    Ported from Metrodroid (https://github.com/metrodroid/metrodroid)

---
+++ farebot-transit-kazan/src/commonMain/kotlin/com/codebutler/farebot/transit/kazan/KazanTransitFactory.kt
@@ -0,0 +1,94 @@
+/*
+ * KazanTransitFactory.kt
+ *
+ * Copyright 2018 Google
+ *
+ * Ported from Metrodroid (https://github.com/metrodroid/metrodroid)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.codebutler.farebot.transit.kazan
+
+import com.codebutler.farebot.base.util.HashUtils
+import com.codebutler.farebot.base.util.NumberUtils
+import com.codebutler.farebot.base.util.byteArrayToInt
+import com.codebutler.farebot.base.util.byteArrayToIntReversed
+import com.codebutler.farebot.base.util.byteArrayToLongReversed
+import com.codebutler.farebot.card.classic.ClassicCard
+import com.codebutler.farebot.card.classic.DataClassicSector
+import com.codebutler.farebot.transit.TransitFactory
+import com.codebutler.farebot.transit.TransitIdentity
+import farebot.farebot_transit_kazan.generated.resources.Res
+import farebot.farebot_transit_kazan.generated.resources.card_name_kazan
+import kotlinx.coroutines.runBlocking
+import kotlin.time.Instant
+import kotlinx.datetime.LocalDate
+import kotlinx.datetime.LocalDateTime
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.atStartOfDayIn
+import kotlinx.datetime.toInstant
+import org.jetbrains.compose.resources.getString
+
+class KazanTransitFactory : TransitFactory<ClassicCard, KazanTransitInfo> {
+
+    override fun check(card: ClassicCard): Boolean {
+        val sector8 = card.getSector(8) as? DataClassicSector ?: return false
+        return HashUtils.checkKeyHash(
+            sector8.keyA, sector8.keyB, "kazan",
+            "0f30386921b6558b133f0f49081b932d",
+            "ec1b1988a2021019074d4304b4aea772"
+        ) >= 0
+    }
+
+    override fun parseIdentity(card: ClassicCard): TransitIdentity {
+        return TransitIdentity.create(
+            runBlocking { getString(Res.string.card_name_kazan) },
+            NumberUtils.zeroPad(getSerial(card), 10)
+        )
+    }
+
+    override fun parseInfo(card: ClassicCard): KazanTransitInfo {
+        val sector8 = card.getSector(8) as DataClassicSector
+        val sector9 = card.getSector(9) as DataClassicSector
+        val block80 = sector8.getBlock(0).data
+        val block82 = sector8.getBlock(2).data
+
+        return KazanTransitInfo(
+            mSerial = getSerial(card),
+            mSub = KazanSubscription(
+                mType = block80[6].toInt() and 0xff,
+                validFrom = parseDate(block80, 7),
+                validTo = parseDate(block80, 10),
+                mCounter = sector9.getBlock(0).data.byteArrayToIntReversed(0, 4)
+            ),
+            mTrip = KazanTrip.parse(block82)
+        )
+    }
+
+    companion object {
+        private val TZ = TimeZone.of("Europe/Moscow")
+
+        private fun getSerial(card: ClassicCard): Long =
+            card.tagId.byteArrayToLongReversed()
+
+        private fun parseDate(raw: ByteArray, off: Int): Instant? {
+            if (raw.byteArrayToInt(off, 3) == 0) return null
+            val year = (raw[off].toInt() and 0xff) + 2000
+            val month = raw[off + 1].toInt() and 0xff
+            val day = raw[off + 2].toInt() and 0xff
+            return LocalDate(year, month, day).atStartOfDayIn(TZ)
+        }
+    }
+}
---
+++ farebot-transit-kazan/src/commonMain/kotlin/com/codebutler/farebot/transit/kazan/KazanTrip.kt
@@ -0,0 +1,60 @@
+/*
+ * KazanTrip.kt
+ *
+ * Copyright 2018 Google
+ *
+ * Ported from Metrodroid (https://github.com/metrodroid/metrodroid)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.codebutler.farebot.transit.kazan
+
+import com.codebutler.farebot.base.util.byteArrayToInt
+import com.codebutler.farebot.transit.TransitCurrency
+import com.codebutler.farebot.transit.Trip
+import kotlin.time.Instant
+import kotlinx.datetime.LocalDateTime
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toInstant
+
+class KazanTrip(
+    override val startTimestamp: Instant
+) : Trip() {
+
+    override val mode: Mode
+        get() = Mode.OTHER
+
+    override val fare: TransitCurrency?
+        get() = null
+
+    companion object {
+        private val TZ = TimeZone.of("Europe/Moscow")
+
+        fun parse(raw: ByteArray): KazanTrip? {
+            if (raw.byteArrayToInt(1, 3) == 0) return null
+            return KazanTrip(parseTime(raw, 1))
+        }
+
+        private fun parseTime(raw: ByteArray, off: Int): Instant {
+            val year = raw[off].toInt() and 0xff
+            val month = raw[off + 1].toInt() and 0xff
+            val day = raw[off + 2].toInt() and 0xff
+            val hour = raw[off + 3].toInt() and 0xff
+            val min = raw[off + 4].toInt() and 0xff
+            return LocalDateTime(year + 2000, month, day, hour, min)
+                .toInstant(TZ)
+        }
+    }
+}
