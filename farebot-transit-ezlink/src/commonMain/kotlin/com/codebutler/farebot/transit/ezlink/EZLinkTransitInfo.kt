9eeb8a62dfd35b18383387f8db197483bdb368a6
Eric Butler <eric@codebutler.com>
2026-02-05 08:53:57 -0500

    Convert farebot-transit-ezlink to Kotlin Multiplatform

---
+++ farebot-transit-ezlink/src/commonMain/kotlin/com/codebutler/farebot/transit/ezlink/EZLinkTransitInfo.kt
@@ -0,0 +1,45 @@
+/*
+ * EZLinkTransitInfo.kt
+ *
+ * Copyright 2011 Sean Cross <sean@chumby.com>
+ * Copyright 2011-2012 Eric Butler <eric@codebutler.com>
+ * Copyright 2012 Toby Bonang
+ *
+ * Ported from Metrodroid (https://github.com/metrodroid/metrodroid)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.codebutler.farebot.transit.ezlink
+
+import com.codebutler.farebot.base.util.StringResource
+import com.codebutler.farebot.transit.TransitBalance
+import com.codebutler.farebot.transit.TransitCurrency
+import com.codebutler.farebot.transit.TransitInfo
+import com.codebutler.farebot.transit.Trip
+
+class EZLinkTransitInfo(
+    override val serialNumber: String?,
+    private val mBalance: Int?,
+    override val trips: List<Trip>,
+    private val stringResource: StringResource,
+) : TransitInfo() {
+
+    override val cardName: String
+        get() = EZLinkData.getCardIssuer(serialNumber, stringResource)
+
+    // This is stored in cents of SGD
+    override val balance: TransitBalance?
+        get() = if (mBalance != null) TransitBalance(balance = TransitCurrency.SGD(mBalance)) else null
+}
---
+++ farebot-transit-ezlink/src/commonMain/kotlin/com/codebutler/farebot/transit/ezlink/EZLinkTrip.kt
@@ -0,0 +1,150 @@
+/*
+ * EZLinkTrip.kt
+ *
+ * Copyright 2011 Sean Cross <sean@chumby.com>
+ * Copyright 2011-2012 Eric Butler <eric@codebutler.com>
+ * Copyright 2012 Victor Heng
+ * Copyright 2012 Toby Bonang
+ * Copyright 2018 Michael Farrell <micolous+git@gmail.com>
+ *
+ * Ported from Metrodroid (https://github.com/metrodroid/metrodroid)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.codebutler.farebot.transit.ezlink
+
+import com.codebutler.farebot.base.util.StringResource
+import com.codebutler.farebot.card.cepas.CEPASTransaction
+import com.codebutler.farebot.transit.Station
+import com.codebutler.farebot.transit.TransitCurrency
+import com.codebutler.farebot.transit.Trip
+import farebot.farebot_transit_ezlink.generated.resources.*
+import kotlin.time.Instant
+
+internal data class EZUserData(
+    val startStation: Station?,
+    val endStation: Station?,
+    val routeName: String,
+) {
+    companion object {
+        fun parse(userData: String, type: CEPASTransaction.TransactionType, stringResource: StringResource): EZUserData {
+            if (type == CEPASTransaction.TransactionType.BUS &&
+                (userData.startsWith("SVC") || userData.startsWith("BUS"))
+            ) {
+                return EZUserData(
+                    startStation = null,
+                    endStation = null,
+                    routeName = stringResource.getString(Res.string.ez_bus_number, userData.substring(3, 7).replace(" ", "")),
+                )
+            }
+            if (type == CEPASTransaction.TransactionType.CREATION) {
+                return EZUserData(Station.nameOnly(userData), null, stringResource.getString(Res.string.ez_first_use))
+            }
+            if (type == CEPASTransaction.TransactionType.RETAIL) {
+                return EZUserData(Station.nameOnly(userData), null, stringResource.getString(Res.string.ez_retail_purchase))
+            }
+
+            val routeName = when (type) {
+                CEPASTransaction.TransactionType.BUS -> stringResource.getString(Res.string.ez_unknown_format, userData)
+                CEPASTransaction.TransactionType.BUS_REFUND -> stringResource.getString(Res.string.ez_bus_refund)
+                CEPASTransaction.TransactionType.MRT -> stringResource.getString(Res.string.ez_mrt)
+                CEPASTransaction.TransactionType.TOP_UP -> stringResource.getString(Res.string.ez_topup)
+                CEPASTransaction.TransactionType.SERVICE -> stringResource.getString(Res.string.ez_service_charge)
+                else -> stringResource.getString(Res.string.ez_unknown_format, type.toString())
+            }
+
+            if (userData.length > 6 && (userData[3] == '-' || userData[3] == ' ')) {
+                val startStationAbbr = userData.substring(0, 3)
+                val endStationAbbr = userData.substring(4, 7)
+                return EZUserData(
+                    EZLinkData.getStation(startStationAbbr),
+                    EZLinkData.getStation(endStationAbbr),
+                    routeName,
+                )
+            }
+            return EZUserData(Station.nameOnly(userData), null, routeName)
+        }
+    }
+}
+
+class EZLinkTrip(
+    private val transaction: CEPASTransaction,
+    private val cardName: String,
+    private val stringResource: StringResource,
+) : Trip() {
+
+    override val startTimestamp: Instant
+        get() = Instant.fromEpochSeconds(transaction.timestamp.toLong())
+
+    override val routeName: String
+        get() = EZUserData.parse(transaction.userData, transaction.type, stringResource).routeName
+
+    override val humanReadableRouteID: String?
+        get() = transaction.userData
+
+    override val fare: TransitCurrency?
+        get() = if (transaction.type == CEPASTransaction.TransactionType.CREATION) {
+            null
+        } else {
+            TransitCurrency.SGD(-transaction.amount)
+        }
+
+    override val startStation: Station?
+        get() = EZUserData.parse(transaction.userData, transaction.type, stringResource).startStation
+
+    override val endStation: Station?
+        get() = EZUserData.parse(transaction.userData, transaction.type, stringResource).endStation
+
+    override val mode: Mode
+        get() = getMode(transaction.type)
+
+    override val agencyName: String
+        get() = getAgencyName(transaction.type, cardName, isShort = false, stringResource)
+
+    override val shortAgencyName: String
+        get() = getAgencyName(transaction.type, cardName, isShort = true, stringResource)
+
+    companion object {
+        fun getMode(type: CEPASTransaction.TransactionType): Mode = when (type) {
+            CEPASTransaction.TransactionType.BUS,
+            CEPASTransaction.TransactionType.BUS_REFUND -> Mode.BUS
+            CEPASTransaction.TransactionType.MRT -> Mode.METRO
+            CEPASTransaction.TransactionType.TOP_UP -> Mode.TICKET_MACHINE
+            CEPASTransaction.TransactionType.RETAIL,
+            CEPASTransaction.TransactionType.SERVICE -> Mode.POS
+            else -> Mode.OTHER
+        }
+
+        fun getAgencyName(
+            type: CEPASTransaction.TransactionType,
+            cardName: String,
+            isShort: Boolean,
+            stringResource: StringResource,
+        ): String = when (type) {
+            CEPASTransaction.TransactionType.BUS,
+            CEPASTransaction.TransactionType.BUS_REFUND -> stringResource.getString(Res.string.ezlink_agency_bus)
+            CEPASTransaction.TransactionType.CREATION,
+            CEPASTransaction.TransactionType.TOP_UP,
+            CEPASTransaction.TransactionType.SERVICE ->
+                if (isShort && cardName == stringResource.getString(Res.string.ezlink_issuer_ezlink)) {
+                    stringResource.getString(Res.string.ezlink_agency_ez)
+                } else {
+                    cardName
+                }
+            CEPASTransaction.TransactionType.RETAIL -> stringResource.getString(Res.string.ezlink_agency_pos)
+            else -> stringResource.getString(Res.string.ezlink_agency_smrt)
+        }
+    }
+}
